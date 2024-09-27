bf1d65fcccae6364667543e4d5c85d55592520ad
Moh7mmedEhab <mohammed.ehab.23062@gmail.com>
2024-09-27 23:52:20 +0300

    Update main.js

--- JS/main.js
+++ JS/main.js
@@ -1,10 +1,20 @@
 let timer = document.querySelector("#countdown");
 
+let head = document.querySelector("#date");
+
+let week_days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+let day = new Date().getDay();
+
 let year = new Date().getFullYear();
+
+;
 
 setInterval(() => {
 
     let time = new Date(`Sep, 30, ${year}, 00:00:00`).getTime();
+
+    let time_cpy = new Date(`Sep, 30, ${year}, 00:00:00`).getDay();
 
     let now = new Date().getTime();
 
@@ -16,9 +26,13 @@ setInterval(() => {
 
         time = new Date(`Sep, 30, ${year}, 00:00:00`).getTime();
 
+        time_cpy = new Date(`Sep, 30, ${year}, 00:00:00`).getDay();
+
         dis = time - now;
 
     }
+
+    head.innerText = `${week_days[time_cpy]} | 30-9-${year}`
 
     let days = Math.floor(dis / (1000 * 60 * 60 * 24));
 
@@ -30,4 +44,4 @@ setInterval(() => {
 
     timer.innerText = `${days.toString().padStart(2, "0")}d : ${(hours % 24).toString().padStart(2, "0")}h : ${(minutes % 60).toString().padStart(2, "0")}m : ${(seconds % 60).toString().padStart(2, "0")}s`;
 
-}, 1);
+}, 10);
